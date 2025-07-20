9b1638e48d80c5ef876c6a20680e0dbd3d287f30
HiagoSilva <hiagosilvaanjos7@gmail.com>
2025-07-20 12:12:55 -0300

    feat: add `sign up form`

---
+++ src/app/authentication/components/sign-up-form.tsx
@@ -0,0 +1,210 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const registerSchema = z
+  .object({
+    name: z.string().trim().min(2, { message: "Nome obrigatório" }),
+    email: z.email({ message: "E-mail inválido" }),
+    password: z
+      .string()
+      .trim()
+      .min(8, { message: "Senha Inválida!" })
+      .refine((val) => /[a-z]/.test(val), {
+        message: "Senha Inválida!",
+      })
+      .refine((val) => /[A-Z]/.test(val), {
+        message: "Senha Inválida!",
+      })
+      .refine((val) => /\d/.test(val), {
+        message: "Senha Inválida!",
+      })
+      .refine((val) => /[\W_]/.test(val), {
+        message: "Senha Inválida!",
+      }),
+    confirmPassword: z.string().min(8, { message: "Confirme sua senha" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
+    path: ["confirmPassword"],
+  });
+
+const SignUpForm = () => {
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof registerSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <CardHeader>
+            <CardTitle>Criar conta</CardTitle>
+            <CardDescription>Crie uma conta para continuar</CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu nome completo" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu e-mail " {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        type="password"
+                        placeholder="Digite sua senha "
+                        {...field}
+                        value={passwordValue}
+                        onChange={(e) => {
+                          field.onChange(e);
+                          setPasswordValue(e.target.value);
+                        }}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                  <ul className="mt-2 space-y-1 text-xs">
+                    <li
+                      className={
+                        /[a-z]/.test(passwordValue)
+                          ? "text-green-600"
+                          : "text-red-600"
+                      }
+                    >
+                      • Pelo menos uma letra minúscula
+                    </li>
+                    <li
+                      className={
+                        /[A-Z]/.test(passwordValue)
+                          ? "text-green-600"
+                          : "text-red-600"
+                      }
+                    >
+                      • Pelo menos uma letra maiúscula
+                    </li>
+                    <li
+                      className={
+                        /\d/.test(passwordValue)
+                          ? "text-green-600"
+                          : "text-red-600"
+                      }
+                    >
+                      • Pelo menos um número
+                    </li>
+                    <li
+                      className={
+                        /[\W_]/.test(passwordValue)
+                          ? "text-green-600"
+                          : "text-red-600"
+                      }
+                    >
+                      • Pelo menos um caractere especial (ex: #, $, &, ...)
+                    </li>
+                    <li
+                      className={
+                        passwordValue.length >= 8
+                          ? "text-green-600"
+                          : "text-red-600"
+                      }
+                    >
+                      • Pelo menos 8 caracteres
+                    </li>
+                  </ul>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Comfirme sua senha</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Digite sua senha novamente "
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+
+          <CardFooter>
+            <Button type="submit" className="w-full cursor-pointer">
+              Criar conta
+            </Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  );
+};
+
+export default SignUpForm;
---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import SignUpForm from "./components/sign-up-form";
+
+// const loginSchema = z.object({
+//   email: z.email({ message: "E-mail inválido" }),
+//   password: z
+//     .string()
+//     .min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
+// });
+
+const AuthenticationPage = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login" className="cursor-pointer">
+            Login
+          </TabsTrigger>
+          <TabsTrigger value="register" className="cursor-pointer">
+            Criar conta
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>Faça login para continuar.</CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Button>Entrar</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="register">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
